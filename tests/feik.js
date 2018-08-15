1e798b387da1ae6a9819702b86240aa77db7a1b8
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-08-14 23:42:56 -0300

    Test available namespaces and methods

--- tests/feik.js
+++ tests/feik.js
@@ -1,10 +1,35 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
 
 const Fk = require('../');
 
 describe('Test API', function() {
-  it('Feik instance should be an object', function() {
-    const feik = new Fk();
-    expect(feik).to.be.a('object');
+  beforeEach(function() {
+    this.feik = new Fk();
+  });
+
+  it('Expect the feik instance to be an object', function() {
+    expect(this.feik).to.be.a('object');
+  });
+
+  it('Expect the feik instance has the "name" namespace', function() {
+    expect(this.feik.name).to.be.a('object');
+  });
+
+  it('Expect the feik instance has the "internet" namespace', function() {
+    expect(this.feik.internet).to.be.a('object');
+  });
+
+  it('Expect the "name" namespace has the "firstName" method', function () {
+    expect(this.feik.name.firstName).to.be.a('function');
+  });
+
+  it('Expect the "name" namespace has the "lastName" method', function () {
+    expect(this.feik.name.lastName).to.be.a('function');
+  });
+
+  it('Expect the "internet" namespace has the "email" method', function () {
+    expect(this.feik.internet.email).to.be.a('function');
   });
 });
